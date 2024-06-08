36ed623610697d172e3ed689b21c957651d79f8d
sam <155526957+Invinciblecode@users.noreply.github.com>
2024-06-08 22:49:37 +0100

    .s

--- src/server.js
+++ src/server.js
@@ -3,35 +3,20 @@ import mongoose from 'mongoose'
 const app = express()
 import dotenv from 'dotenv'
 dotenv.config()
-import ejs from 'ejs'
-import user from '../models/user.js'
-// user route
-import addUserRoute from "./routes/user/add-user.js"
-import editUserRoute from './routes/user/edit-user.js'
-import deleteUserRoute from './routes/user/delete-user.js'
-// blog route  
+// blog routes
 import postBlogRoute from './routes/blog/postBlog.js'
 import listBlogsRoute from './routes/blog/listBlogs.js'
 import editBlogRoute from './routes/blog/editBlog.js'
 import deleteBlogRoute from './routes/blog/deleteBlog.js'
 const PORT = process.env.PORT || 3000
-
-//middleware
-// user
+//middlewares - routes
 app.use(express.json())
-app.use(addUserRoute)
-app.use(editUserRoute)
-app.use(deleteUserRoute) 
-// blog 
 app.use(postBlogRoute)
 app.use(listBlogsRoute)
 app.use(editBlogRoute)
 app.use(deleteBlogRoute)
 
-
-
-
-
+//database
 mongoose.connect(process.env.DBURL)
 .then( () =>{
     console.log('connected to the database')
@@ -39,12 +24,6 @@ mongoose.connect(process.env.DBURL)
 .catch( (err) =>{
     console.log(err)
 })
-
-
-
-
-
-
 
 app.listen( PORT , () =>{
     console.log('server is running on port 3000')
